18bacc4c7c75a6da585f376aebf55c3a80cc1b99
Michał Pasternak <mpasternak@MacBook-Pro-Michala.local>
2017-04-30 11:24:48 +0200

    Więcej coverage

--- tests/test_drunken_child_in_the_fog.py
+++ tests/test_drunken_child_in_the_fog.py
@@ -11,7 +11,7 @@ import os
 
 import pytest
 
-from drunken_child_in_the_fog.core import DrunkenChildInTheFog
+from drunken_child_in_the_fog.core import DrunkenChildInTheFog, NoSuchElement
 
 
 @pytest.fixture
@@ -30,3 +30,17 @@ def test_content(test_file):
     assert e.lines().vertical().count() == 0
     assert e.text().count() > 0
     assert e.containing_text("Hello").count() == 1
+    assert e.containing_text("Hello").first().text.startswith("Hello")
+    with pytest.raises(NoSuchElement):
+        assert e.containing_text("Hello").second()
+
+    page = document.get_pages()[0]
+    assert e.inside(0, 0, page.width, page.height).count() > 0
+    assert page.inside(0, 0, page.width, page.height).count() > 0
+    assert page.containing_text("Hello").count() == 1
+
+    e = e.all()[0]
+    assert e.position_in_document() > 0
+
+    assert str(e) != ""
+
