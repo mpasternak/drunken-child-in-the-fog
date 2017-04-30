0c530c104e66406de9236c6daaec940086c126be
Michał Pasternak <mpasternak@MacBook-Pro-Michala.local>
2017-04-30 11:36:51 +0200

    Bump version: 0.1.0 → 0.1.1

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ test_requirements = [
 
 setup(
     name='drunken_child_in_the_fog',
-    version='0.1.0',
+    version='0.1.1',
     description="Funky PDF parser API, using PDFMiner.six",
     long_description=readme + '\n\n' + history,
     author="Michał Pasternak",
