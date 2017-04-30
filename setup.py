9075beb96fd41b36f5c53e58ecf33015fe177ddf
Michał Pasternak <mpasternak@MacBook-Pro-Michala.local>
2017-04-30 22:43:04 +0200

    Bump version: 0.1.2 → 0.2.0

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ test_requirements = [
 
 setup(
     name='drunken_child_in_the_fog',
-    version='0.1.2',
+    version='0.2.0',
     description="PDF parser API inspired by Django QuerySet, using "
                 "PDFMiner.six",
     long_description=readme + '\n\n' + history,
